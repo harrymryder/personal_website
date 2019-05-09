3a73c351a34ec35d8ebd8121bbf9006aa5c9e3ce
Harry Ryder <harrymryder@gmail.com>
2019-05-09 12:10:49 +0000

    css

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,6 +1,12 @@
-let arrow = document.querySelector('.down-arrow')
-
-arrow.addEventListener('click', (e) => {
+let downarrow = document.querySelector('.down-arrow')
+downarrow.addEventListener('click', (e) => {
   let welcome = document.querySelector('.large-content-page-left')
   welcome.scrollIntoView({ block: 'start',  behavior: 'smooth' })
 })
+
+let uparrow = document.querySelector('.up-arrow')
+uparrow.addEventListener('click', (e) => {
+  let header = document.querySelector('.header')
+  header.scrollIntoView({ block: 'start',  behavior: 'smooth' })
+})
+
