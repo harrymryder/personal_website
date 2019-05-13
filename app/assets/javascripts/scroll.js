7a63a597f40c2967ddb1827f58fd0bfec1f07155
Harry Ryder <harrymryder@gmail.com>
2019-05-13 13:52:32 +0000

    pushed

--- app/assets/javascripts/scroll.js
+++
@@ -1,5 +0,0 @@
-let uparrow = document.querySelector('.up-arrow')
-uparrow.addEventListener('click', (e) => {
-  let header = document.querySelector('.header')
-  header.scrollIntoView({ block: 'start',  behavior: 'smooth' })
-})
